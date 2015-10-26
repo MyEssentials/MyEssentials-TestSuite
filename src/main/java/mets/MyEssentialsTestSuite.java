afd98d562d05eb549ac107d4d959e2cb4e62ef66
AfterWind <afterwinddesu@gmail.com>
2015-10-26 20:00:52 +0200

    Added dependencies and main class.

---
+++ src/main/java/mets/MyEssentialsTestSuite.java
@@ -0,0 +1,29 @@
+package mets;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "MyEssentials-TestSuite", version = "0.00r")
+public class MyEssentialsTestSuite {
+
+    @Mod.Instance
+    public static MyEssentialsTestSuite instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent ev) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent ev) {
+
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPostInitializationEvent ev) {
+
+    }
+
+}
