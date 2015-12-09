be05a2d2d27f40126f2df61f4fb4489f4e03121d
AfterWind <afterwinddesu@gmail.com>
2015-12-09 19:48:18 +0200

    Moved the @RunWith to the BaseTest class as well.

--- src/main/java/metest/BaseTest.java
+++ src/main/java/metest/BaseTest.java
@@ -2,10 +2,11 @@ package metest;
 
 import net.minecraft.server.MinecraftServer;
 import org.apache.logging.log4j.Logger;
+import org.junit.runner.RunWith;
 
 import java.io.File;
 
-
+@RunWith(MinecraftRunner.class)
 public class BaseTest {
 
     public MinecraftServer server;
