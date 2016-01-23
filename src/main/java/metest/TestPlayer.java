a606d48b1327a593f8af4af2149209dabe275604
AfterWind <afterwinddesu@gmail.com>
2016-01-23 17:30:41 +0200

    Added TestPlayer class

---
+++ src/main/java/metest/TestPlayer.java
@@ -0,0 +1,29 @@
+package metest;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.IChatComponent;
+import net.minecraftforge.common.util.FakePlayer;
+
+import java.util.UUID;
+
+/**
+ * Modifies some properties and methods of the MinecraftForge's FakePlayer class
+ * to be able to be used as a testing tool.
+ */
+public class TestPlayer extends FakePlayer {
+
+    public TestPlayer(MinecraftServer server, String name) {
+        super(server.worldServers[0], new GameProfile(UUID.randomUUID(), name));
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(int i, String s) {
+        return true;
+    }
+
+    @Override
+    public void addChatMessage(IChatComponent p_145747_1_) {
+
+    }
+}
